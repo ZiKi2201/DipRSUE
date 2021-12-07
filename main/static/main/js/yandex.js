4364d72a07b5bd35d3d136f7ef0b543c357595e7
ZiKi2201 <leonid220197@gmail.com>
2021-12-07 05:10:55 +0300

    create map

---
+++ main/static/main/js/yandex.js
@@ -0,0 +1,28 @@
+
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map-canvas', {
+            center: [47.216735, 39.703829],
+            zoom: 16
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'static/main/images/mIcon.png',
+            // Размеры метки.
+            iconImageSize: [42, 42],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-15, -38]
+        })
+
+        myMap.geoObjects
+        .add(myPlacemark);
+        })
